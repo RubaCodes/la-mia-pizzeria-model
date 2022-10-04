a904b51d584c5c689e98a6c23ea6a585480d39b6
RubaCodes <rubacodes@gmail.com>
2022-10-04 15:11:09 +0100

    aggiunto routing dinamico (id statico)

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,6 +1,5 @@
 ﻿using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -9,17 +8,37 @@ namespace la_mia_pizzeria_static.Controllers
         public IActionResult Index()
         {
             List<Pizza> menu = new List<Pizza>();
-            Pizza margherita = new Pizza("Margherita", "Pomodoro, Mozzarella", "/img/pizza-photos/margherita.jpg", 8.00m);
-            Pizza diavola = new Pizza("Diavola", "Pomodoro, Mozzarella, Salamino piccante", "/img/pizza-photos/diavola.jpg", 10m);
-            Pizza quattroStagioni = new Pizza("4 Stagioni", "Pomodoro, Mozzarella, Prosciutto Cotto, Funghi, Carciofini, Olive Nere", "/img/pizza-photos/quattro-stagioni.jpg", 12m);
-            Pizza quattroFormaggi = new Pizza("4 Formaggi", "Pomodoro, Mozzarella, Gorgonzola, Scamorza, Grana", "/img/pizza-photos/quattro-formaggi.jpg", 9m);
 
+            Pizza margherita = new Pizza(0, "Margherita", "Pomodoro, Mozzarella", "/img/pizza-photos/margherita.jpg", 8.00m);
+            Pizza diavola = new Pizza(1, "Diavola", "Pomodoro, Mozzarella, Salamino piccante", "/img/pizza-photos/diavola.jpg", 10m);
+            Pizza quattroStagioni = new Pizza(2, "4 Stagioni", "Pomodoro, Mozzarella, Prosciutto Cotto, Funghi, Carciofini, Olive Nere", "/img/pizza-photos/quattro-stagioni.jpg", 12m);
+            Pizza quattroFormaggi = new Pizza(3, "4 Formaggi", "Pomodoro, Mozzarella, Gorgonzola, Scamorza, Grana", "/img/pizza-photos/quattro-formaggi.jpg", 9m);
+
+            //commentare gli add  per testare il menu vuoto // funziona
             menu.Add(margherita);
             menu.Add(diavola);
             menu.Add(quattroStagioni);
             menu.Add(quattroFormaggi);
 
             return View(menu);
+        }
+
+        public IActionResult Show(int id) {
+            List<Pizza> menu = new List<Pizza>();
+
+            Pizza margherita = new Pizza(0,"Margherita", "Pomodoro, Mozzarella", "/img/pizza-photos/margherita.jpg", 8.00m);
+            Pizza diavola = new Pizza(1,"Diavola", "Pomodoro, Mozzarella, Salamino piccante", "/img/pizza-photos/diavola.jpg", 10m);
+            Pizza quattroStagioni = new Pizza(2,"4 Stagioni", "Pomodoro, Mozzarella, Prosciutto Cotto, Funghi, Carciofini, Olive Nere", "/img/pizza-photos/quattro-stagioni.jpg", 12m);
+            Pizza quattroFormaggi = new Pizza(3,"4 Formaggi", "Pomodoro, Mozzarella, Gorgonzola, Scamorza, Grana", "/img/pizza-photos/quattro-formaggi.jpg", 9m);
+
+            //commentare gli add  per testare il menu vuoto // funziona
+            menu.Add(margherita);
+            menu.Add(diavola);
+            menu.Add(quattroStagioni);
+            menu.Add(quattroFormaggi);
+
+            return View(menu[id]);
+
         }
     }
 }
--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -2,16 +2,19 @@ namespace la_mia_pizzeria_static.Models
 {
     public class Pizza
     {
+
+        public int PizzaId { get; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? ImgPath { get; set; }
         public decimal? Price { get; set; }
 
-        public Pizza(string name, string description, string imgpath, decimal price) {
+        public Pizza( int id ,string name, string description, string imgpath, decimal price) {
             Name = name;
             Description = description;
             ImgPath = imgpath;
             Price = price;
+            PizzaId = id;
         }
     }
 }
