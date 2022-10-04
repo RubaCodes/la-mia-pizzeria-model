04c777a56886d43ddeb9a90f59bcae545082ed0a
RubaCodes <rubacodes@gmail.com>
2022-10-04 14:20:09 +0100

    Popolato la home con @model

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -8,7 +8,18 @@ namespace la_mia_pizzeria_static.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Pizza> menu = new List<Pizza>();
+            Pizza margherita = new Pizza("Margherita", "Pomodoro, Mozzarella", "/img/pizza-photos/margherita.jpg", 8.00m);
+            Pizza diavola = new Pizza("Diavola", "Pomodoro, Mozzarella, Salamino piccante", "/img/pizza-photos/diavola.jpg", 10m);
+            Pizza quattroStagioni = new Pizza("4 Stagioni", "Pomodoro, Mozzarella, Prosciutto Cotto, Funghi, Carciofini, Olive Nere", "/img/pizza-photos/quattro-stagioni.jpg", 12m);
+            Pizza quattroFormaggi = new Pizza("4 Formaggi", "Pomodoro, Mozzarella, Gorgonzola, Scamorza, Grana", "/img/pizza-photos/quattro-formaggi.jpg", 9m);
+
+            menu.Add(margherita);
+            menu.Add(diavola);
+            menu.Add(quattroStagioni);
+            menu.Add(quattroFormaggi);
+
+            return View(menu);
         }
     }
 }
